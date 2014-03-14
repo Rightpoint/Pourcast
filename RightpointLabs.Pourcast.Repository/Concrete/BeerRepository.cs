a00a3f6af25e4d556069d206f99646d399154bda
Stephen Gilboy <sgilboy@rightpoint.com>
2014-03-14 17:07:49 -0500

    Added slugs on create methods for beer and brewery repositories.

--- RightpointLabs.Pourcast.Repository/Concrete/BeerRepository.cs
+++ RightpointLabs.Pourcast.Repository/Concrete/BeerRepository.cs
@@ -3,13 +3,22 @@ using System.Linq;
 using RightpointLabs.Pourcast.DataModel;
 using RightpointLabs.Pourcast.Repository.Abstract;
 using RightpointLabs.Pourcast.DataModel.Entities;
+using Slugify;
 
 namespace RightpointLabs.Pourcast.Repository.Concrete
 {
     public class BeerRepository : EntityRepository<Beer>, IBeerRepository
     {
+        private readonly SlugHelper _slug;
+
         public BeerRepository(IMongoConnectionHandler<Beer> connectionHandler) : base(connectionHandler)
         {
+        }
+
+        public override void Create(Beer entity)
+        {
+            entity.Slug = _slug.GenerateSlug(entity.Name);
+            base.Create(entity);
         }
 
         public override void Update(Beer entity)
--- RightpointLabs.Pourcast.Repository/Concrete/BreweryRepository.cs
+++ RightpointLabs.Pourcast.Repository/Concrete/BreweryRepository.cs
@@ -1,13 +1,23 @@
 ﻿using RightpointLabs.Pourcast.DataModel;
 using RightpointLabs.Pourcast.DataModel.Entities;
 using RightpointLabs.Pourcast.Repository.Abstract;
+using Slugify;
 
 namespace RightpointLabs.Pourcast.Repository.Concrete
 {
     public class BreweryRepository : EntityRepository<Brewery>, IBreweryRepository
     {
+        private readonly SlugHelper _slug;
+
         public BreweryRepository(IMongoConnectionHandler<Brewery> connectionHandler) : base(connectionHandler)
         {
+            _slug = new SlugHelper();
+        }
+
+        public override void Create(Brewery entity)
+        {
+            entity.Slug = _slug.GenerateSlug(entity.Name);
+            base.Create(entity);
         }
 
         public override void Update(Brewery entity)
