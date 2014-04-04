dafb0420421e9c2da6490a945763a89a128ee4f9
ssartell <ssartell@gmail.com>
2014-04-03 21:06:33 -0500

    Undo repo change

--- RightpointLabs.Pourcast.Domain/Repositories/IBeerRepository.cs
+++ RightpointLabs.Pourcast.Domain/Repositories/IBeerRepository.cs
@@ -1,12 +1,9 @@
 ﻿namespace RightpointLabs.Pourcast.Domain.Repositories
 {
-    using System.Collections.Generic;
-
     using RightpointLabs.Pourcast.Domain.Models;
 
     public interface IBeerRepository
     {
-        IEnumerable<Beer> GetAll();
         Beer GetById(string id);
     }
 }
