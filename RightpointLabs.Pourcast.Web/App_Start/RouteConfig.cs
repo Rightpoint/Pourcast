4ec5b63cb483e9c716556ecebf8686ce6b55d931
Stephen Gilboy <sgilboy@rightpoint.com>
2014-04-14 22:04:11 -0500

    Added Admin Area

--- RightpointLabs.Pourcast.Web/App_Start/RouteConfig.cs
+++ RightpointLabs.Pourcast.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace RightpointLabs.Pourcast.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[]{ "RightpointLabs.Pourcast.Web.Controllers"}
             );
         }
     }
