d34c3dc7668227f451f2842d2b5ff58b2fad49ff
Stephen Gilboy <sgilboy@rightpoint.com>
2014-04-15 19:26:08 -0500

    Adding create beer files

---
+++ RightpointLabs.Pourcast.Web/Scripts/app/viewModel/pourcast.beerVM.js
@@ -0,0 +1,27 @@
+﻿/*
+    Brewery View Model
+*/
+(function (app, $, ko, toast, moment) {
+    app.BeerVM = app.BeerVM|| function () {
+        var self = this;
+        self.createVisible = ko.observable(false);
+        self.newBeer = ko.observable();
+
+        self.create = function () {
+            self.newBeer(new app.Beer());
+            self.createVisible(true);
+        };
+
+        self.save = function () {
+            var brewery = self.newBrewery();
+
+            if (brewery.name()) {
+                self.breweries.push(brewery);
+                toast.success("Created " + brewery.name());
+                self.newBrewery(null);
+                self.createVisible(false);
+            }
+        };
+
+    };
+}(window.pourcast = window.pourcast || {}, jQuery, ko, toastr, moment));
