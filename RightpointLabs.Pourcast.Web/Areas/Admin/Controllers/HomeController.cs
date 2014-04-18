074cabfdedac2fb09a50b51927561407d74914d7
Stephen Gilboy <sgilboy@rightpoint.com>
2014-04-17 20:56:52 -0500

    Added Admin Homepage View

--- RightpointLabs.Pourcast.Web/Areas/Admin/Controllers/HomeController.cs
+++ RightpointLabs.Pourcast.Web/Areas/Admin/Controllers/HomeController.cs
@@ -3,16 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RightpointLabs.Pourcast.Application.Orchestrators.Abstract;
+using RightpointLabs.Pourcast.Web.Areas.Admin.Models;
 
 namespace RightpointLabs.Pourcast.Web.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITapOrchestrator _tapOrchestrator;
+        private readonly IKegOrchestrator _kegOrchestrator;
+        private readonly IBeerOrchestrator _beerOrchestrator;
+
+        public HomeController(ITapOrchestrator tapOrchestrator, IKegOrchestrator kegOrchestrator,
+            IBeerOrchestrator beerOrchestrator)
+        {
+            if(tapOrchestrator == null) throw new ArgumentNullException("tapOrchestrator");
+            if(kegOrchestrator == null) throw new ArgumentNullException("kegOrchestrator");
+            if(beerOrchestrator == null) throw new ArgumentNullException("beerOrchestrator");
+            _kegOrchestrator = kegOrchestrator;
+        }
+
         //
         // GET: /Admin/Home/
         public ActionResult Index()
         {
-            return View();
+            // TODO Talk about how to figure out which keg is on which tap
+            var kegsOnTap = new KegsOnTap();
+            var kegs = _kegOrchestrator.GetKegsOnTap();
+            var count = 0;
+            foreach (var keg in kegs)
+            {
+                if (count == 0)
+                {
+                    kegsOnTap.LeftKeg = new OnTapKeg()
+                    {
+                        Keg = keg,
+                        Beer = _beerOrchestrator.GetById(keg.BeerId)
+                    };
+                }
+                else
+                {
+                    kegsOnTap.RightKeg = new OnTapKeg()
+                    {
+                        Keg = keg,
+                        Beer = _beerOrchestrator.GetById(keg.BeerId)
+                    };                    
+                }
+                count++;
+            }
+
+            return View("Index", kegsOnTap);
         }
 	}
 }
---
+++ RightpointLabs.Pourcast.Web/Areas/Admin/Models/KegsOnTap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RightpointLabs.Pourcast.Web.Areas.Admin.Models
+{
+    public class KegsOnTap
+    {
+        public OnTapKeg RightKeg { get; set; }
+        public OnTapKeg LeftKeg { get; set; }
+    }
+}
