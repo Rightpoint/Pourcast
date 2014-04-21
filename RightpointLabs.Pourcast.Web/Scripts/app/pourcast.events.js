ee474e620160c7389569c72446b7f4cec44504cb
ssartell <ssartell@gmail.com>
2014-04-21 18:19:25 -0500

    Add signalr reconnect logic

--- RightpointLabs.Pourcast.Web/Scripts/app/pourcast.events.js
+++ RightpointLabs.Pourcast.Web/Scripts/app/pourcast.events.js
@@ -1,6 +1,20 @@
-﻿(function (app, $, ko, toast, moment) {
+﻿(function (app, $) {
     app.events = app.events || function () {
         $.connection.hub.start();
+
+        var timeout = 0;
+
+        // try reconnect if disconnected
+        $.connection.eventsHub.disconnected(function() {
+            setTimeout(function () {
+                $.connection.eventsHub.start();
+                timeout++;
+            }, Math.pow(2, timeout) * 1000);
+        });
+
+        $.connection.hub.reconnected(function () {
+            timeout = 0;
+        });
 
         return {
             on: function (event, callback) {
@@ -12,4 +26,4 @@
         }
     }();
 
-}(window.pourcast = window.pourcast || {}, jQuery, ko, toastr, moment));
+}(window.pourcast = window.pourcast || {}, jQuery));
