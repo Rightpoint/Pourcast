3467a714cd28a1c173ad818b213ea0fa2ebf3242
ssartell <ssartell@gmail.com>
2014-04-23 21:33:52 -0500

    Add beer on tap api methods

---
+++ RightpointLabs.Pourcast.Application/Payloads/BeerOnTap.cs
@@ -0,0 +1,15 @@
+﻿namespace RightpointLabs.Pourcast.Application.Payloads
+{
+    using RightpointLabs.Pourcast.Domain.Models;
+
+    public class BeerOnTap
+    {
+        public Keg Keg { get; set; }
+
+        public Tap Tap { get; set; }
+
+        public Beer Beer { get; set; }
+
+        public Brewery Brewery { get; set; }
+    }
+}
---
+++ RightpointLabs.Pourcast.Web/Controllers/Api/BeerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace RightpointLabs.Pourcast.Web.Controllers.Api
+{
+    using RightpointLabs.Pourcast.Application.Orchestrators.Abstract;
+    using RightpointLabs.Pourcast.Application.Payloads;
+
+    public class BeerController : ApiController
+    {
+        private readonly IBeerOrchestrator _beerOrchestrator;
+
+        public BeerController(IBeerOrchestrator beerOrchestrator)
+        {
+            if (beerOrchestrator == null) throw new ArgumentNullException("beerOrchestrator");
+
+            _beerOrchestrator = beerOrchestrator;
+        }
+
+        // GET api/<controller>
+        public IEnumerable<BeerOnTap> Get()
+        {
+            return _beerOrchestrator.GetBeersOnTap();
+        }
+
+        public BeerOnTap Get(string tapId)
+        {
+            return _beerOrchestrator.GetBeerOnTap(tapId);
+        }
+    }
+}
