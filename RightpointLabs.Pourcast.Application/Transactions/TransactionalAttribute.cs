1bdf048d0071233dca01bd0d95e2b094bbdb6157
ssartell <ssartell@gmail.com>
2014-04-26 08:46:33 -0500

    Add transactional attribute

---
+++ RightpointLabs.Pourcast.Application/Transactions/TransactionalAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RightpointLabs.Pourcast.Application.Transactions
+{
+    using System.Transactions;
+
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.InterceptionExtension;
+
+    public class TransactionalAttribute : HandlerAttribute
+    {
+        public override ICallHandler CreateHandler(IUnityContainer container)
+        {
+            return new TransactionCallHandler();
+        }
+    }
+
+    public class TransactionCallHandler : ICallHandler
+    {
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextHandlerDelegate getNext)
+        {
+            IMethodReturn result;
+
+            using (var scope = new TransactionScope())
+            {
+                result = getNext()(input, getNext);
+
+                scope.Complete();
+            }
+
+            return result;
+        }
+
+        public int Order { get; set; }
+    }
+}
