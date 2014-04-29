e70b788259ccbf34c8d881ad9019ef9fef4385e7
ssartell <ssartell@gmail.com>
2014-04-28 20:26:27 -0500

    Clean up unity

--- RightpointLabs.Pourcast.Web/App_Start/UnityConfig.cs
+++ RightpointLabs.Pourcast.Web/App_Start/UnityConfig.cs
@@ -46,7 +46,7 @@ namespace RightpointLabs.Pourcast.Web
             var database = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Mongo"].ProviderName;
 
             container.RegisterType<IMongoConnectionHandler, MongoConnectionHandler>(
-                new PerRequestLifetimeManager(),
+                new ContainerControlledLifetimeManager(),
                 new InjectionConstructor(connectionString, database));
 
             // orchestrators
@@ -71,20 +71,15 @@ namespace RightpointLabs.Pourcast.Web
                 WithLifetime.Custom<PerRequestLifetimeManager>,
                 getInjectionMembers: t => new InjectionMember[]
                 {
-                    //new InterceptionBehavior<PolicyInjectionBehavior>(),
-                    //new Interceptor<InterfaceInterceptor>()
+                    new InterceptionBehavior<PolicyInjectionBehavior>(),
+                    new Interceptor<InterfaceInterceptor>()
                 });
             container.RegisterTypes(
                 AllClasses.FromLoadedAssemblies().Where(
                   t => t.Namespace == "RightpointLabs.Pourcast.Infrastructure.Persistence.Collections"),
                 WithMappings.FromAllInterfaces,
                 WithName.Default,
-                WithLifetime.Custom<ContainerControlledLifetimeManager>,
-                getInjectionMembers: t => new InjectionMember[]
-                {
-                    //new InterceptionBehavior<PolicyInjectionBehavior>(),
-                    //new Interceptor<InterfaceInterceptor>()
-                });
+                WithLifetime.Custom<ContainerControlledLifetimeManager>);
 
             // domain services
             container.RegisterType<IEmailService, SmtpEmailService>(new PerRequestLifetimeManager());
