970354614ac3b7eb961a33f0960bde007499ed9a
decluecm <cdeclue@rightpoint.com>
2014-04-28 20:28:52 -0500

    Theoritically, the repourter will report. If it ever gets all the way hooked up, hardware wise.

--- RightpointLabs.Pourcast.Repourter/HttpMessageWriter.cs
+++ RightpointLabs.Pourcast.Repourter/HttpMessageWriter.cs
@@ -8,26 +8,35 @@ namespace RightpointLabs.Pourcast.Repourter
 {
     public static class HttpMessageWriter
     {
-        public static void SendMessageAsync(string tapId, double ounces)
+        public static void SendStartAsync(int tapId)
+        {
+            var thread = new Thread(() => SendMessage(tapId));
+            thread.Start();
+        }
+
+        public static void SendStopAsync(int tapId, double ounces)
         {
             var thread = new Thread(() => SendMessage(tapId, ounces));
             thread.Start();
         }
 
-        private static void SendMessage(string tapId, double ounces)
+        private static void SendMessage(int tapId, double volume = 0.0, bool isStart = true)
         {
             WiFlyGSX WifiModule = new WiFlyGSX();
             WifiModule.EnableDHCP();
-            WifiModule.JoinNetwork("Netduino");
+            WifiModule.JoinNetwork("The Password is 1234", 0, WiFlyGSX.AuthMode.WPA2_PSK, "I lied, it's not.");
 
             // Creates a socket
-            SimpleSocket Socket = new WiFlySocket("www.netmftoolbox.com", 80, WifiModule);
+            SimpleSocket Socket = new WiFlySocket("/rpc-cdeclue/", 80, WifiModule);
 
             // Connects to the socket
             Socket.Connect();
-
+            var controller = "/api/reportertest/"; // TODO: change
+            //var controller = "/api/tap/";
+            var action = isStart ? "startpourt" : "stoppour";
+            var parameters = "?tapId=" + tapId + (isStart ? "" : "&volume=" + volume);
             // Does a plain HTTP request
-            Socket.Send("GET /helloworld/ HTTP/1.1\r\n");
+            Socket.Send("GET " + controller + action + parameters + " HTTP/1.1\r\n");
             Socket.Send("Host: " + Socket.Hostname + "\r\n");
             Socket.Send("Connection: Close\r\n");
             Socket.Send("\r\n");
