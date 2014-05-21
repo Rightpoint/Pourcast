1b1f699a7ea953a1cface3e53ecd14ae5476df08
Stephen Gilboy <sgilboy@rightpoint.com>
2014-05-21 17:51:03 -0500

    try parameterless constructor for azure

--- RightpointLabs.Pourcast.Web/Controllers/HomeController.cs
+++ RightpointLabs.Pourcast.Web/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace RightpointLabs.Pourcast.Web.Controllers
 
         private readonly IIdentityOrchestrator _identityOrchestrator;
 
+        public HomeController() { }
+
         public HomeController(ITapOrchestrator tapOrchestrator, IIdentityOrchestrator identityOrchestrator)
         {
             _tapOrchestrator = tapOrchestrator;
