2a86a910a8064a27e4bfc34ced32959fb6890bca
ssartell <ssartell@gmail.com>
2014-06-01 16:50:53 -0500

    more style fixes

--- RightpointLabs.Pourcast.Web/Scripts/app/model/keg.js
+++ RightpointLabs.Pourcast.Web/Scripts/app/model/keg.js
@@ -3,14 +3,20 @@
         var self = this;
 
         self.id = ko.observable(kegJSON.Id);
-        self.percentRemaining = ko.observable(Math.floor(kegJSON.PercentRemaining * 100) + '<span class="symbol">%</span>');
+        self.percentRemaining = ko.observable(Math.floor(kegJSON.PercentRemaining * 100));
         self.isEmpty = ko.observable(kegJSON.IsEmpty);
         self.isPouring = ko.observable(kegJSON.IsPouring);
         self.capacity = ko.observable(kegJSON.Capacity);
         self.beer = ko.observable(beer);
 
+        self.percentRemainingStyle = ko.computed(function () {
+            return self.percentRemaining() + '%';
+        });
+        self.percentRemainingHtml = ko.computed(function() {
+            return self.percentRemaining() + '<span class="symbol">%</span>';
+        });
         self.percentRemainingBubble = ko.computed(function() {
-            return (kegJSON.PercentRemaining * 100) > 25 ? "high" : "low";
+            return self.percentRemaining() > 25 ? "high" : "low";
         });
 
         events.on("PourStarted", self.pourStarted);
