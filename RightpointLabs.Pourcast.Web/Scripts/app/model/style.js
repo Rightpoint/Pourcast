a22f7debf3418a0f46fa98d769f73e0eb7ed4336
ssartell <ssartell@gmail.com>
2014-06-01 15:16:34 -0500

    Fix colors

--- RightpointLabs.Pourcast.Web/Scripts/app/model/style.js
+++ RightpointLabs.Pourcast.Web/Scripts/app/model/style.js
@@ -1,1 +1,14 @@
-﻿
+﻿define(['jquery', 'ko'], function ($, ko) {
+    function Style(styleJSON, brewery) {
+        var self = this;
+
+        self.id = ko.observable(styleJSON.Id);
+        self.name = ko.observable(styleJSON.Name);
+        self.color = ko.observable(styleJSON.Color);
+        self.glass = ko.observable(styleJSON.Glass);
+
+        self.brewery = ko.observable(brewery);
+    };
+
+    return Style;
+});
