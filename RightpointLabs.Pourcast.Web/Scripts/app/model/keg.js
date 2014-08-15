cf49ad51c329c66b26d0ba21b24fa1f5dcf9756d
ssartell <ssartell@gmail.com>
2014-08-15 14:26:57 -0500

    Fix keg percentage bug

--- RightpointLabs.Pourcast.Web/Scripts/app/model/keg.js
+++ RightpointLabs.Pourcast.Web/Scripts/app/model/keg.js
@@ -1,9 +1,9 @@
-﻿define(['jquery', 'ko', 'app/events'], function($, ko, events) {
+﻿define(['jquery', 'ko', 'app/events'], function ($, ko, events) {
     function Keg(kegJSON, beer) {
         var self = this;
 
         self.id = ko.observable(kegJSON.Id);
-        self.percentRemaining = ko.observable((kegJSON.PercentRemaining * 100));
+        self.percentRemaining = ko.observable((kegJSON.PercentRemaining * 100).toFixed(1));
         self.isEmpty = ko.observable(kegJSON.IsEmpty);
         self.isPouring = ko.observable(kegJSON.IsPouring);
         self.capacity = ko.observable(kegJSON.Capacity);
@@ -16,25 +16,25 @@
         self.percentRemainingStyle = ko.computed(function () {
             return Math.floor(self.percentRemaining()) + '%';
         });
-        self.percentRemainingHtml = ko.computed(function() {
-            return parseFloat(self.percentRemaining()) + '<span class="symbol">%</span>';
+        self.percentRemainingHtml = ko.computed(function () {
+            return parseFloat(self.percentRemaining()).toFixed(1) + '<span class="symbol">%</span>';
         });
-        self.percentRemainingClass = ko.computed(function() {
+        self.percentRemainingClass = ko.computed(function () {
             return self.isLow() ? "low" : "high";
         });
 
-        events.on("PourStarted", function(e) {
+        events.on("PourStarted", function (e) {
             console.log("PourStarted");
             if (e.KegId === self.id()) {
                 self.isPouring(true);
             }
         });
-        events.on("PourStopped", function(e) {
+        events.on("PourStopped", function (e) {
             console.log("PourStopped");
 
             if (e.KegId === self.id()) {
                 self.isPouring(false);
-                self.percentRemaining((e.PercentRemaining * 100));
+                self.percentRemaining((e.PercentRemaining * 100).toFixed(1));
             }
         });
     };
