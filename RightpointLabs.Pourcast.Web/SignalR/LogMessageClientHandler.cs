fd0fea1fd7dc85ee684484069c84c360da0b3e2b
Jonathan Rupp <jorupp@gmail.com>
2014-09-20 19:30:30 -0500

    Add admin screen to watch events
    /Admin/Status

---
+++ RightpointLabs.Pourcast.Web/SignalR/LogMessageClientHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace RightpointLabs.Pourcast.Web.SignalR
+{
+    using Microsoft.AspNet.SignalR.Infrastructure;
+
+    using RightpointLabs.Pourcast.Application.Transactions;
+    using RightpointLabs.Pourcast.Domain.Events;
+
+    public class LogMessageClientHandler : IEventHandler<LogMessage>
+    {
+        private readonly IConnectionManager _connectionManager;
+
+        public LogMessageClientHandler(IConnectionManager connectionManager)
+        {
+            _connectionManager = connectionManager;
+        }
+
+        public void Handle(LogMessage domainEvent)
+        {
+            var context = _connectionManager.GetHubContext<EventsHub>();
+
+            TransactionExtensions.WaitForTransactionCompleted(() => context.Clients.All.LogMessage(domainEvent));
+        }
+    }
+}
