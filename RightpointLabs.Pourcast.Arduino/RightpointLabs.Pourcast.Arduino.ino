a31bff6405cf78e9b1763bd8ee752f1c2aaf972a
jorupp <jorupp@gmail.com>
2014-10-09 22:34:34 -0500

    Trying to encapsulate a bit more
    This is in preparation to explore letting the Arduino handle everything

--- RightpointLabs.Pourcast.Arduino/RightpointLabs.Pourcast.Arduino.ino
+++ RightpointLabs.Pourcast.Arduino/RightpointLabs.Pourcast.Arduino.ino
@@ -1,90 +1,130 @@
-volatile long _tap1Pulses = 0;
-volatile long _tap2Pulses = 0;
 const long _startThreshold = 30;
 const long _continueThreshold = 5;
+class Reporter {
+public: 
+  virtual void ReportStop(long pulses);
+  virtual void ReportContinue(long pulses);
+  virtual void ReportStart(long pulses);
+  virtual void ReportIgnore(long pulses);
+};
+
+class Tap {
+public: 
+  Tap(Reporter* reporter);
+  inline void HandlePulse();
+  void Loop(byte cycleNumber);
+private:
+  Reporter* _reporter;
+  volatile long _pulses;
+  long _lastPulses;
+};
+
+Tap::Tap(Reporter* reporter) {
+  _reporter = reporter;
+}
+void Tap::HandlePulse() {
+  _pulses++;
+}
+void Tap::Loop(byte cycle) {
+  long pulses = _pulses;
+  if(_lastPulses != 0 && cycle % 10 == 0) {
+    // in progress and on the second
+    if(pulses - _lastPulses < _continueThreshold) {
+      // complete
+      _pulses = 0;
+      _lastPulses = 0;
+      _reporter->ReportStop(pulses);
+    } 
+    else {
+      // continuing
+      _lastPulses = pulses;
+      _reporter->ReportContinue(pulses);
+    }
+  } 
+  else if(_lastPulses == 0) {
+    // not in progress
+    if(pulses >= _startThreshold) {
+      // starting
+      _lastPulses = pulses;
+      _reporter->ReportStart(pulses);
+    } 
+    else if(pulses > 0 && cycle % 10 == 0) {
+      // ignored
+      _pulses = 0;
+      _reporter->ReportIgnore(pulses);
+    }
+  }
+}
+
+class SerialReporter: public Reporter {
+public:
+  SerialReporter(byte number);
+  virtual void ReportStop(long pulses);
+  virtual void ReportContinue(long pulses);
+  virtual void ReportStart(long pulses);
+  virtual void ReportIgnore(long pulses);
+private:
+  byte _number;
+};
+
+SerialReporter::SerialReporter(byte number) { 
+  _number = number;
+}
+
+void SerialReporter::ReportStop(long pulses){
+  Serial.print("STOP ");
+  Serial.println(_number, DEC);
+  Serial.print(" ");
+  Serial.println(pulses, DEC);
+}
+void SerialReporter::ReportContinue(long pulses) {
+  Serial.print("CONTINUE ");
+  Serial.println(_number, DEC);
+  Serial.print(" ");
+  Serial.println(pulses, DEC);
+}
+void SerialReporter::ReportStart(long pulses){
+  Serial.print("START ");
+  Serial.println(_number, DEC);
+  Serial.print(" ");
+  Serial.println(pulses, DEC);
+}
+void SerialReporter::ReportIgnore(long pulses){
+  Serial.print("IGNORE ");
+  Serial.println(_number, DEC);
+  Serial.print(" ");
+  Serial.println(pulses, DEC);
+}
+
+
+Tap* tap1;
+Tap* tap2;
 
 // prep - initialize serial (TX on pin 1) and wire up the interrupts for pulses from the taps (pins 2 and 3)
 void setup() {
   Serial.begin(9600);
   while(!Serial);
+  tap1 = new Tap(new SerialReporter(1));
   attachInterrupt(0, tap1Pulse, RISING);
+  tap2 = new Tap(new SerialReporter(2));
   attachInterrupt(1, tap2Pulse, RISING);
 }
 
 // handle interrupt pulses from the taps
 void tap1Pulse() {
-  ++_tap1Pulses;
+  tap1->HandlePulse();
 }
 void tap2Pulse() {
-  ++_tap2Pulses;
+  tap2->HandlePulse();
 }
 
 // main loop - once a second, check the accumulated pulse counts and send the START/STOP/CONTINUE/IGNORE message as necessary
 // send an ALIVE message every loop to assist debugging
 void loop() {
-  long lastTap1Pulses = 0;
-  long lastTap2Pulses = 0;
-  int cycle = 0;
+  byte cycle = 0;
   while(true) {
-    long tap1Pulses = _tap1Pulses;
-    if(lastTap1Pulses != 0 && cycle % 10 == 0) {
-      // in progress and on the second
-      if(tap1Pulses - lastTap1Pulses < _continueThreshold) {
-        // complete
-        _tap1Pulses = 0;
-        lastTap1Pulses = 0;
-        Serial.print("STOP 1 ");
-        Serial.println(tap1Pulses, DEC);
-      } else {
-        // continuing
-        Serial.print("CONTINUE 1 ");
-        Serial.println(tap1Pulses, DEC);
-        lastTap1Pulses = tap1Pulses;
-      }
-    } else if(lastTap1Pulses == 0) {
-      // not in progress
-      if(tap1Pulses >= _startThreshold) {
-        // starting
-        Serial.print("START 1 ");
-        Serial.println(tap1Pulses, DEC);
-        lastTap1Pulses = tap1Pulses;
-      } else if(tap1Pulses > 0 && cycle % 10 == 0) {
-        // ignored
-        _tap1Pulses = 0;
-        Serial.print("IGNORE 1 ");
-        Serial.println(tap1Pulses, DEC);
-      }
-    }
-    
-    long tap2Pulses = _tap2Pulses;
-    if(lastTap2Pulses != 0 && cycle % 10 == 0) {
-      // in progress and on the second
-      if(tap2Pulses - lastTap2Pulses < _continueThreshold) {
-        // complete
-        _tap2Pulses = 0;
-        lastTap2Pulses = 0;
-        Serial.print("STOP 2 ");
-        Serial.println(tap2Pulses, DEC);
-      } else {
-        // continuing
-        Serial.print("CONTINUE 2 ");
-        Serial.println(tap2Pulses, DEC);
-        lastTap2Pulses = tap2Pulses;
-      }
-    } else if(lastTap2Pulses == 0) {
-      // not in progress
-      if(tap2Pulses >= _startThreshold) {
-        // starting
-        Serial.print("START 2 ");
-        Serial.println(tap2Pulses, DEC);
-        lastTap2Pulses = tap2Pulses;
-      } else if(tap2Pulses > 0 && cycle % 20 == 0) {
-        // ignored
-        _tap2Pulses = 0;
-        Serial.print("IGNORE 2 ");
-        Serial.println(tap2Pulses, DEC);
-      }
-    }
+    tap1->Loop(cycle);
+    tap2->Loop(cycle);
 
     if(cycle % 10 == 0) {
       Serial.println("ALIVE");
@@ -93,3 +133,4 @@ void loop() {
     delay(100);
   }
 }
+
