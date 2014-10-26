a00e79a199a8035d57fb8ebf1f4f5e1aed5835fa
Sean Sartell <ssartell@rightpoint.com>
2014-10-25 20:41:10 -0500

    clean up

--- RightpointLabs.Pourcast.Web/Scripts/app/components/percent/viewModel.js
+++ RightpointLabs.Pourcast.Web/Scripts/app/components/percent/viewModel.js
@@ -6,11 +6,8 @@
         self.percentRemaining = ko.computed(function() {
             return (ko.unwrap(model.percentRemaining) * 100).toFixed(1);
         });
-        self.isLow = ko.computed(function () {
-            return ko.unwrap(model.isLow);
-        });
         self.levelCssClass = ko.computed(function () {
-            return ko.unwrap(self.isLow) ? "low" : "high";
+            return ko.unwrap(model.isLow) ? "low" : "high";
         });
     };
 
