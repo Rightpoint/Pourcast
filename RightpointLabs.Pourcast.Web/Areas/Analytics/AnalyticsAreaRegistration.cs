6baa9bbd3f19ca795358d5ce40e90ce74979df61
Stephen Gilboy <sgilboy@rightpoint.com>
2014-10-30 11:11:31 -0500

    Added the analytics area

---
+++ RightpointLabs.Pourcast.Web/Areas/Analytics/AnalyticsAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace RightpointLabs.Pourcast.Web.Areas.Analytics
+{
+    public class AnalyticsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Analytics";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Analytics_default",
+                "Analytics/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "RightpointLabs.Pourcast.Web.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
---
+++ RightpointLabs.Pourcast.Web/Areas/Analytics/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RightpointLabs.Pourcast.Web.Areas.Analytics.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Analytics/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
