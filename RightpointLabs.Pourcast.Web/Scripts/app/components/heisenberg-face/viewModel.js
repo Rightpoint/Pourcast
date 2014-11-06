2aa558ab48d4893c4f6cb4d007f5d009e1d27bc6
Sean Sartell <ssartell@rightpoint.com>
2014-11-05 19:43:13 -0600

    Add mustaches

---
+++ RightpointLabs.Pourcast.Web/Scripts/app/components/heisenberg-face/viewModel.js
@@ -0,0 +1,11 @@
+﻿define(['ko', 'text!app/components/heisenberg-face/template.html'], function (ko, htmlString) {
+
+    function HeisenbergFace(model) {
+        var self = this;
+    };
+
+    return {
+        viewModel: HeisenbergFace,
+        template: htmlString
+    };
+});
--- RightpointLabs.Pourcast.Web/Scripts/app/components/tap/viewModel.js
+++ RightpointLabs.Pourcast.Web/Scripts/app/components/tap/viewModel.js
@@ -15,8 +15,15 @@
         self.resolver.register('keg-body', 'kegBody');
         self.resolver.register('face', 'face');
         self.resolver.register('beer', 'beer');
-        self.resolver.register('hat', 'onKegBody');
         self.resolver.register('percent', 'bits');
+
+        // heisenberg
+        self.resolver.register('heisenberg-face', 'face');
+        self.resolver.register('heisenberg-hat', 'onKegBody');
+
+        // borat
+        self.resolver.register('borat-face', 'face');
+        self.resolver.register('borat-flag', 'outsideRing');
     };
 
     return {
