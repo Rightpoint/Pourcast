2ff5e0d1e96e76d907c2975d7aa30c639e86c2ac
Jonathan Rupp <jorupp@gmail.com>
2015-02-11 18:03:42 -0600

    Broadcast pictures via SignalR

---
+++ RightpointLabs.Pourcast.Web/SignalR/PictureTakenClientHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace RightpointLabs.Pourcast.Web.SignalR
+{
+    using Microsoft.AspNet.SignalR.Infrastructure;
+
+    using RightpointLabs.Pourcast.Application.Transactions;
+    using RightpointLabs.Pourcast.Domain.Events;
+
+    public class PictureTakenClientHandler : IEventHandler<PictureTaken>
+    {
+        private readonly IConnectionManager _connectionManager;
+
+        public PictureTakenClientHandler(IConnectionManager connectionManager)
+        {
+            _connectionManager = connectionManager;
+        }
+
+        public void Handle(PictureTaken domainEvent)
+        {
+            var context = _connectionManager.GetHubContext<EventsHub>();
+
+            TransactionExtensions.WaitForTransactionCompleted(() => context.Clients.All.PictureTaken(domainEvent));
+        }
+    }
+}
