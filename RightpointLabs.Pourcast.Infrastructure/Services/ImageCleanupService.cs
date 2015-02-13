70e49cfe35e53ab6682f977d7fd8c51b6435468a
Jonathan Rupp <jorupp@gmail.com>
2015-02-12 18:33:43 -0600

    log the image in/out
    For future optimizations :)

--- RightpointLabs.Pourcast.Infrastructure/Services/ImageCleanupService.cs
+++ RightpointLabs.Pourcast.Infrastructure/Services/ImageCleanupService.cs
@@ -2,14 +2,17 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using AForge.Imaging.Filters;
+using log4net;
 using RightpointLabs.Pourcast.Domain.Services;
 
 namespace RightpointLabs.Pourcast.Infrastructure.Services
 {
     public class ImageCleanupService : IImageCleanupService
     {
+        private static ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         public string CleanUpImage(string rawDataUrl)
         {
             string contentType;
@@ -21,7 +24,11 @@ namespace RightpointLabs.Pourcast.Infrastructure.Services
                 using (var ms2 = new MemoryStream())
                 {
                     image.Save(ms2, ImageFormat.Jpeg);
-                    return string.Concat("data:image/jpeg;base64,", Convert.ToBase64String(ms2.ToArray()));
+                    var newDataUrl = string.Concat("data:image/jpeg;base64,", Convert.ToBase64String(ms2.ToArray()));
+                    log.DebugFormat("Converted {0} to {1}", rawDataUrl.Length, newDataUrl.Length);
+                    log.DebugFormat("  Input: {0}", rawDataUrl);
+                    log.DebugFormat("  Output: {0}", newDataUrl);
+                    return newDataUrl;
                 }
             }
         }
