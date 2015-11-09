3e0d2cc2cc0c9e7f63257b8afc83724dc673ea25
Jonathan Rupp <jrupp@rightpoint.com>
2015-11-09 08:04:41 -0600

    Add some more code that can be used to help debug the image recognition and overlay process

--- RightpointLabs.Pourcast.Tests/Infrastructure/Services/FaceRecognitionServiceTest.cs
+++ RightpointLabs.Pourcast.Tests/Infrastructure/Services/FaceRecognitionServiceTest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Drawing;
 using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RightpointLabs.Pourcast.Domain.Services;
 using RightpointLabs.Pourcast.Infrastructure.Services;
 
 namespace RightpointLabs.Pourcast.Tests.Infrastructure.Services
@@ -25,6 +29,50 @@ namespace RightpointLabs.Pourcast.Tests.Infrastructure.Services
             if (!string.IsNullOrEmpty(finalUrl))
                 Console.WriteLine(finalUrl);
             Console.WriteLine("Users: " + string.Join(", ", users) + ", added overlay: " + addedOverlay);
+        }
+
+        //[TestMethod]
+        public void TestOverlay()
+        {
+	        var files = Directory.GetFiles(@"C:\Users\jrupp\Desktop\mustaches");
+	
+	        var apiKey="";
+	        var apiSecret="";
+	        var ns="";
+	
+            var rawDataUrl = string.Concat("data:image/jpeg;base64,", Convert.ToBase64String(File.ReadAllBytes(@"C:\Users\jrupp\Desktop\out\source1.png")));
+	
+	        var i = 0;
+	        var outBase = @"C:\Users\jrupp\Desktop\out";
+	        foreach(var file in files) {
+		        var svc = new FaceRecognitionService(apiKey, apiSecret, ns, null, new FakeOverlay(file));
+		        string intermediateUrl, finalUrl, contentType;
+		        bool addedOverlay;
+		        svc.ProcessImage(rawDataUrl, out intermediateUrl, out finalUrl, out addedOverlay);
+		        File.WriteAllBytes(Path.Combine(outBase, "img-" + (i++) + ".png"), GetDataFromUrl(finalUrl, out contentType));
+	        }
+        }
+
+        class FakeOverlay : IOverlayImageProvider {
+	        string _filename;
+	        public FakeOverlay(string filename) {
+		        _filename = filename;
+	        }
+	        public Image GetRandomOverlayImage() {
+		        return Image.FromFile(_filename);
+	        }
+        }
+
+        private byte[] GetDataFromUrl(string dataUrl, out string contentType)
+        {
+          // https://gist.github.com/vbfox/484643
+          var match = Regex.Match(dataUrl, @"data:image/(?<type>.+?);base64,(?<data>.+)");
+          var type = match.Groups["type"].Value;
+          var base64Data = match.Groups["data"].Value;
+          var binData = Convert.FromBase64String(base64Data);
+
+          contentType = "image/" + type;
+          return binData;
         }
     }
 }
