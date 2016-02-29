f4181e5a3e5ff216a4d11fd0538ce0f99feab4df
Jonathan Rupp <jrupp@rightpoint.com>
2016-02-29 11:36:37 -0600

    Log info about authentication checks

--- RightpointLabs.Pourcast.Web/PourcastRoleProvider.cs
+++ RightpointLabs.Pourcast.Web/PourcastRoleProvider.cs
@@ -1,4 +1,7 @@
-﻿namespace RightpointLabs.Pourcast.Web
+﻿using System.Reflection;
+using log4net;
+
+namespace RightpointLabs.Pourcast.Web
 {
     using System.Linq;
     using System.Web.Mvc;
@@ -10,6 +13,7 @@
 
     public class PourcastRoleProvider : RoleProvider
     {
+        private static ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private IIdentityOrchestrator _identityOrchestrator
         {
             get
@@ -21,11 +25,13 @@
 
         public override bool IsUserInRole(string username, string roleName)
         {
+            log.DebugFormat("PRP.IsUserInRole: {0}, {1}", username, roleName);
             return _identityOrchestrator.IsUserInRole(username, roleName);
         }
 
         public override string[] GetRolesForUser(string username)
         {
+            log.DebugFormat("PRP.GetRolesForUser: {0}", username);
             return _identityOrchestrator.GetRolesForUser(username).Select(x => x.Name).ToArray();
         }
 
@@ -58,6 +64,7 @@
 
         public override string[] GetUsersInRole(string roleName)
         {
+            log.DebugFormat("PRP.GetUsersInRole: {0}", roleName);
             return _identityOrchestrator.GetUsersInRole(roleName).Select(x => x.Username).ToArray();
         }
 
@@ -68,6 +75,7 @@
 
         public override string[] FindUsersInRole(string roleName, string usernameToMatch)
         {
+            log.DebugFormat("PRP.FindUsersInRole: {0}, {1}", roleName, usernameToMatch);
             throw new System.NotImplementedException();
         }
 
